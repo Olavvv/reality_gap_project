075895067915362622ee22fa8ce5671393078e4a
Teaprime <42270507+Teaprime@users.noreply.github.com>
2024-10-05 01:56:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 import time
 from datetime import datetime
 
-import brax.mjx.pipeline
-from etils import epath
-import functools
-from IPython.display import HTML
-from typing import Any, Dict, Sequence, Tuple, Union
-import os
-from ml_collections import config_dict
+#import brax.mjx.pipeline
+#from etils import epath
+#import functools
+#from IPython.display import HTML
+#from typing import Any, Dict, Sequence, Tuple, Union
+#import os
+#from ml_collections import config_dict
 
 
 import jax
 from jax import numpy as jp
 import numpy as np
-from flax.training import orbax_utils
-from flax import struct
-from matplotlib import pyplot as plt
+#from flax.training import orbax_utils
+#from flax import struct
+#from matplotlib import pyplot as plt
 import mediapy as media
-from orbax import checkpoint as ocp
+#from orbax import checkpoint as ocp
 
 import mujoco
 from mujoco import viewer
 from mujoco import mjx
 
-from brax import base
-from brax import envs
-from brax import math
-from brax.base import Base, Motion, Transform
-from brax.envs.base import Env, PipelineEnv, State
-from brax.mjx.base import State as MjxState
-from brax.training.agents.ppo import train as ppo
-from brax.training.agents.ppo import networks as ppo_networks
-from brax.io import html, mjcf, model
+#from brax import base
+#from brax import envs
+#from brax import math
+#from brax.base import Base, Motion, Transform
+#from brax.envs.base import Env, PipelineEnv, State
+#from brax.mjx.base import State as MjxState
+#from brax.training.agents.ppo import train as ppo
+#from brax.training.agents.ppo import networks as ppo_networks
+#from brax.io import html, mjcf, model
 
-import glfw
+#import glfw
 
 from random import random as rand
 from math import sin
@@ -124,7 +124,7 @@ def run_sim(m, d, duration, controller, fps=60, view=None, scene_option=None, do
 
 
 #########################################################################
-batch_size = 16
+batch_size = 2
 duration = 10
 mj_model = mujoco.MjModel.from_xml_path("./qutee.xml")
 mj_data = mujoco.MjData(mj_model)
